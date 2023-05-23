913aed2faf8b7fb6b671e6330f6342bc93354b7c
Piyush Acharya <acharyapiyush1@outlook.com>
2023-05-22 18:26:48 -0700

    Add basic algorithms

---
+++ Week 1/ParseFastQ.py
@@ -0,0 +1,8 @@
+def readGenome(filename):
+    genome = ''
+    with open(filename, 'r') as f:
+        for line in f:
+            # ignore header line with genome information
+            if not line[0] == '>':
+                genome += line.rstrip()
+    return genome
---
+++ Week 1/ReverseComplement.py
@@ -0,0 +1,6 @@
+def reverseComplement(s):
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N'}
+    t = ''
+    for base in s:
+        t = complement[base] + t
+    return t
